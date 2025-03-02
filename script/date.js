94eca9b7624e6e748ac68845b0570f0da6481500
ImTariquL <imtariqul21@gmail.com>
2025-03-02 16:35:03 +0600

    qna section

--- script/date.js
+++ script/date.js
@@ -1,12 +1,14 @@
 function date() {
     let today = new Date();
-    let tDay=today.getDay();
-    let day = today.getDate();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
+    let day= days[today.getDay()];
+    let date = today.getDate();
     let month = today.toLocaleString('en-US', { month: 'short' }); 
     let year = today.getFullYear();
 
-    let dayText = ` ${tDay},  ${month} ${day} ${year}`;
+    let dayText = `${month} ${date} ${year}`;
 
+    document.getElementById("day").innerText = day; 
     document.getElementById("date").innerText = dayText;
 }
  
